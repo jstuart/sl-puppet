97222a9e5ad70535e048dd1a2805d7f11c4af3d5
James Stuart <james@stuart.name>
2015-05-19 14:49:16 -0400

    r10k structure

---
+++ site.pp
@@ -0,0 +1,10 @@
+hiera_include('classes')
+
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
