ed757e28c17c4b101aa96f6be35b2cd83b8b65a3
James Stuart <james@stuart.name>
2015-05-19 15:21:54 -0400

    defer to foreman

--- site.pp
+++ site.pp
@@ -1,8 +1,6 @@
-hiera_include('classes')
 
 if versioncmp($::puppetversion,'3.6.1') >= 0 {
-
-  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+ $allow_virtual_packages = hiera('allow_virtual_packages',false)
 
   Package {
     allow_virtual => $allow_virtual_packages,
